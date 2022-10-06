83d74ebe981e6ddea15ca258722e0338a0995de9
AliSeyyedi <aliakbarseyyedi@gmail.com>
2022-10-06 21:07:20 +0330

    new file:   rate.py

---
+++ rate.py
@@ -0,0 +1,12 @@
+
+endpoint = '/api/v1/margin/market?currency=ETH&term=7'
+
+def getEthRate():
+  EthRate = requests.get(url + endpoint).json()
+  EthRate = (EthRate.get('data'))
+  EthRate = EthRate[0]
+  EthRate = json.dumps(EthRate)
+  EthRate = json.loads(EthRate)
+  EhtRate = EthRate["dailyIntRate"]
+  EhtRate = str(EhtRate)
+  return EhtRate
