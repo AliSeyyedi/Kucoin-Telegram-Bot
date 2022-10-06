1245185b2827133940382cfcc0cca3aafa8e5c5f
AliSeyyedi <aliakbarseyyedi@gmail.com>
2022-10-06 22:43:25 +0330

    new file:   account.py 	modified:   main.py 	modified:   rate.py

--- rate.py
+++ rate.py
@@ -1,9 +1,10 @@
 import requests
+import account
 
 endpoint = '/api/v1/margin/market?currency=ETH&term=7'
 
 def getEthRate():
-  EthRate = requests.get(url + endpoint).json()
+  EthRate = requests.get(account.url + endpoint).json()
   EthRate = (EthRate.get('data'))
   EthRate = EthRate[0]
   EthRate = json.dumps(EthRate)
