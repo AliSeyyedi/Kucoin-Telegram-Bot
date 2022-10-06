9f55bfcecb690d0393b3c1d901c6ac5e27d02ef5
AliSeyyedi <aliakbarseyyedi@gmail.com>
2022-10-07 02:09:23 +0330

    modified:   lend.py

--- lend.py
+++ lend.py
@@ -3,23 +3,28 @@ import requests
 import account
 import json
 
+import time
+import pybase64 as base64
+import hmac
+import account
+import hashlib
+
 endpoint = '/api/v1/margin/lend'
 method = 'POST'
 
 def PlaceEthLendOrder():
   params = {
-    'currency'      : 'ETH' ,
-    'size'          : 'All' ,
-    'dailyIntRate'  : '0.002',
-    'term'          : '7'
+  'currency'      : 'ETH' ,
+  'size'          : '0.05' ,
+  'dailyIntRate'  : '0.002',
+  'term'          : '7'
   }
   data = json.dumps(params)
   headers = H.getHeaders(method, endpoint+data)
-  PlaceEthLendOrder = requests.post(account.url + endpoint, data=data, headers=headers).json()
-  print(PlaceEthLendOrder)
-  # PlaceEthLendOrder = (PlaceEthLendOrder.get('data'))
-  # PlaceEthLendOrder = json.dumps(PlaceEthLendOrder)
-  # PlaceEthLendOrder = json.loads(PlaceEthLendOrder)
-  # PlaceEthLendOrder = PlaceEthLendOrder["orderId"]
-  # PlaceEthLendOrder = str(PlaceEthLendOrder)
+  PlaceEthLendOrder = requests.post(account.url + '/api/v1/margin/lend', headers=headers, data=data).json()
+  PlaceEthLendOrder = (PlaceEthLendOrder.get('data'))
+  PlaceEthLendOrder = json.dumps(PlaceEthLendOrder)
+  PlaceEthLendOrder = json.loads(PlaceEthLendOrder)
+  PlaceEthLendOrder = PlaceEthLendOrder["orderId"]
+  PlaceEthLendOrder = str(PlaceEthLendOrder)
   return PlaceEthLendOrder
