1ebcfb04dde846d76a2dfe779b291d5509106784
AliSeyyedi <aliakbarseyyedi@gmail.com>
2022-10-08 00:41:38 +0330

    modified:   account.py

--- account.py
+++ account.py
@@ -1,11 +1,11 @@
 # real acc
-# api_key = '6331daf3b72af700018adee2'
-# api_secret = '020c7d27-158c-4cec-bc9e-6ff0b03ea848'
-# api_passphrase = 'newLending'
-# url = 'https://api.kucoin.com'
+api_key = '6331daf3b72af700018adee2'
+api_secret = '020c7d27-158c-4cec-bc9e-6ff0b03ea848'
+api_passphrase = 'newLending'
+url = 'https://api.kucoin.com'
 
 # test acc
-api_key = '6336c39529c69200011ee3e3'
-api_secret = '5e74c38d-4623-417e-9b38-517d2909a462'
-api_passphrase = 'sandbox'
-url = 'https://openapi-sandbox.kucoin.com'
+# api_key = '6336c39529c69200011ee3e3'
+# api_secret = '5e74c38d-4623-417e-9b38-517d2909a462'
+# api_passphrase = 'sandbox'
+# url = 'https://openapi-sandbox.kucoin.com'
